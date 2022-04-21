10dc3380f9278e5c6b49f67c6d21dd8c51d21fca
MrClue <gayssuck123@gmail.com>
2022-04-21 12:20:18 +0200

    Update maxHeap.py

--- Heaps/maxHeap.py
+++ Heaps/maxHeap.py
@@ -8,26 +8,25 @@ heapify(heap)
 
 # Adding items to the heap using heappush
 # function by multiplying them with -1
-heappush(heap, -1 * 6)
-heappush(heap, -1 * 12)
-heappush(heap, -1 * 13)
-heappush(heap, -1 * 4)
+heappush(heap, -1 * 10)
 heappush(heap, -1 * 8)
-heappush(heap, -1 * 14)
+heappush(heap, -1 * 6)
+heappush(heap, -1 * 3)
+heappush(heap, -1 * 7)
+heappush(heap, -1 * 4)
 heappush(heap, -1 * 5)
+heappush(heap, -1 * 1)
+heappush(heap, -1 * 2)
+heappush(heap, -1 * 9)
 
 # printing the value of maximum element
-print("Head value of heap : "+str(-1 * heap[0]))
-
-# printing the elements of the heap
-print("The heap elements : ")
-for i in heap:
-	print(-1 * i, end = ' ')
-print("\n")
+print("Head value of heap: "+str(-1 * heap[0]))
 
 element = heappop(heap)
 
 # printing the elements of the heap
-print("The heap elements : ")
+print("The heap elements: ")
 for i in heap:
 	print(-1 * i, end = ' ')
+
+
