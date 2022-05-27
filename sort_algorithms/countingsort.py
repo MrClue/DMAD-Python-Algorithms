8df0c298b56a826f3a30a80c1b19a35d5ab2b27b
sanjr19 <samuel-njri@hotmail.com>
2022-05-27 21:17:23 +0200

    add C array to countingsort working final

--- sort_algorithms/countingsort.py
+++ sort_algorithms/countingsort.py
@@ -1,66 +1,40 @@
-from itertools import count
-from operator import index
-from numpy import array
+def count_sort(arr):
+    max_element = int(max(arr))
+    min_element = int(min(arr))
+    range_of_elements = max_element - min_element + 1
 
-def countingSort(array):
-    size = len(array)
-    output = [0] * size
+    # Create a count array to store count of individual
+    # elements and initialize count array as 0
+    count_arr = [0 for _ in range(range_of_elements)]
+    output_arr = [0 for _ in range(len(arr))]
+ 
+    # Store count of each character
+    for i in range(0, len(arr)):
+        count_arr[arr[i]-min_element] += 1
+ 
+    # Change count_arr[i] so that count_arr[i] now contains actual
+    # position of this element in output array
+    for i in range(1, len(count_arr)):
+        count_arr[i] += count_arr[i-1]
 
-    c1 = []
-    c2 = []
-    c_final = []
+ 
+    # Build the output character array
+    for i in range(len(arr)-1, -1, -1):
+        output_arr[count_arr[arr[i] - min_element] - 1] = arr[i]
+        count_arr[arr[i] - min_element] -= 1
+ 
+    # Copy the output array to arr, so that arr now
+    # contains sorted characters
+    for i in range(0, len(arr)):
+        arr[i] = output_arr[i]
 
-    # Initialize count array
-    count = [0] * size
 
-    # Store the count of each elements in count array
-    for i in range(0, size):
-        count[array[i]] += 1
-
-    # Printing C1
-    for i in range(0, size - 1):
-        c1.append(count[i])
-    
-    print("C: (stored counts of A)")
-    print(c1)
-
-    # Store the cummulative count
-    for i in range(1, size):
-        count[i] += count[i - 1]
-
-    # Printing C2
-    for i in range(0, size - 1):
-        c2.append(count[i])
-    
-    print("C: (cummulative count)")
-    print(c2,"\n")
-
-    # Find the index of each element of the original array in count array
-    # place the elements in output array
-    i = size - 1
-    while i >= 0:
-        output[count[array[i]] - 1] = array[i]
-        count[array[i]] -= 1
-        i -= 1
-
-    # Copy the sorted elements into original array
-    for i in range(0, size):
-        array[i] = output[i]
-
-    # Printing working array C
-    for i in range(0, size - 1):
-        c_final.append(count[i])
-    
-    print("Final working array C")
-    print(c_final)
-        
-
-# OUTPUT
-data = [3, 1, 4, 3, 5, 0, 3, 1]
-
-print("A: (start input)")
-print(data)
-
-countingSort(data)
-print("Sorted Array in Ascending Order: ")
-print(data)
+    print("C array final:",count_arr)
+ 
+    return arr
+ 
+ 
+# Driver program to test above function
+arr = [3,1,4,3,5,0,3,1]
+ans = count_sort(arr)
+print("Sorted character array is " + str(ans))
