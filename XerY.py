eb4016b0409005fd1a2a10d08506593b16730a7d
MrClue <Motar19@student.sdu.dk>
2022-06-09 14:05:01 +0200

    comments

--- XerY.py
+++ XerY.py
@@ -6,12 +6,29 @@ def log(a):
     log_version = 10
     return math.log(a, log_version)
 
+# defining n to some random large number (to compute results)
+n = 123456789
+
 # Compare run-times using asymptotic notation
 
 def c1(x, y):
     # x er O(y)
     print("Case 1: x er O(y)")
     print(str(x)+" er <=("+str(y)+"): " + str(x <= y) + "\n")
+#    if (x >= n and y >= n): # x og y har "n" condition
+#        print(str(x)+" er <=("+str(y)+"): " + str(x <= y) + "\n")
+
+#    if (x >= n and y < n): # x = "n" condition, y = konstant
+#        print("False" + "\n")
+
+#    if (x < n and y >= n): # x = konstant, y = "n" condition
+#        print("TRUE" + "\n")
+
+#    if (x < n and y < n): # x og y = konstanter
+#        print("TRUE" + "\n")
+
+#    else: # original
+#        print(str(x)+" er <=("+str(y)+"): " + str(x <= y) + "\n")
 
 def c2(x, y):
     # x er Ω(y) --- \u03A9
@@ -33,15 +50,13 @@ def c5(x, y):
     print("Case 5: x er w(y)")
     print(str(x)+" er >("+str(y)+"): " + str(x > y) + "\n")
 
-# defining n to some random large number (to compute results)
-n = 123
-
 # Noter:
 # - "kvadratrod(x)" er "math.sqrt(x)"
 # - "n^2" er "n**2"
+# - Konstanter "(1,2,3...)" har altid samme voksehastighed! De er altid lavest voksehastighed i forhold til andre!
 
 # OUTPUT:
-#c1(x, y) # x er O(y)
+#c1(x, y) # x er O(y) --- Konstanter (1,2,3...) er altid = TRUE, (konstant voksehastighed)!
 #c2(x, y) # x er Ω(y)
 #c3(x, y) # x er ϴ(y)
 #c4(x, y) # x er o(y)
